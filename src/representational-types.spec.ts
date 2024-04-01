2c224e05e80196da3fc986ce1ef004105dde9b38
Anton Zamay <tony.zamay@mail.ru>
2024-04-01 13:27:50 +0200

    finish tests for currently added representational types

--- src/representational-types.spec.ts
+++ src/representational-types.spec.ts
@@ -1,5 +1,55 @@
 import { testType } from '../utils/test-utils';
-import { JSONObject, JSONValue } from './representational-types';
+import {
+  GeoJSONFeature,
+  GeoJSONFeatureCollection,
+  GeoJSONGeometry,
+  GeoJSONLineString,
+  GeoJSONMultiLineString,
+  GeoJSONMultiPoint,
+  GeoJSONMultiPolygon,
+  GeoJSONPoint,
+  GeoJSONPolygon,
+  HTTPRequestBody,
+  HTTPResponse,
+  JSONObject,
+  JSONPatch,
+  JSONPatchOperation,
+  JSONValue,
+  NullableJSONValue,
+} from './representational-types';
+
+/**
+ * Helpers
+ */
+
+interface PointProperties {
+  name: string;
+  elevation?: number;
+}
+
+interface AreaProperties {
+  name: string;
+  area: number;
+  protected: boolean;
+}
+
+const simpleJSONData: JSONObject = { key: 'value' };
+const complexJSONData: JSONObject = {
+  user: 'john_doe',
+  details: { age: 30, active: true },
+  tags: ['admin', 'user'],
+};
+
+// More detailed response body example
+interface UserResponse extends JSONObject {
+  id: number;
+  username: string;
+  profile: { age: number; active: boolean };
+}
+
+/**
+ * Tests
+ */
 
 // @dts-jest:group JSONObject
 {
@@ -41,4 +91,388 @@ import { JSONObject, JSONValue } from './representational-types';
     null,
     { keyInArray: 'objectValue' },
   ]);
+}
+
+// @dts-jest:group NullableJSONValue
+{
+  // @dts-jest:pass:snap
+  testType<NullableJSONValue>(undefined);
+
+  // @dts-jest:pass:snap
+  testType<NullableJSONValue>('stringValue');
+
+  // @dts-jest:pass:snap
+  testType<NullableJSONValue>(null);
+
+  // @dts-jest:pass:snap
+  testType<NullableJSONValue>(123);
+}
+
+// @dts-jest:group JSONPatchOperation
+{
+  // @dts-jest:pass:snap
+  testType<JSONPatchOperation>({
+    op: 'add',
+    path: '/a/b/c',
+    value: 'stringValue',
+  });
+
+  // @dts-jest:pass:snap
+  testType<JSONPatchOperation>({ op: 'remove', path: '/a/b/c' });
+
+  // @dts-jest:pass:snap
+  testType<JSONPatchOperation>({ op: 'replace', path: '/a/b/c', value: 123 });
+
+  // @dts-jest:pass:snap
+  testType<JSONPatchOperation>({ op: 'move', from: '/a/b/c', path: '/d/e/f' });
+
+  // @dts-jest:pass:snap
+  testType<JSONPatchOperation>({ op: 'copy', from: '/a/b/c', path: '/d/e/f' });
+
+  // @dts-jest:pass:snap
+  testType<JSONPatchOperation>({ op: 'test', path: '/a/b/c', value: true });
+}
+
+// @dts-jest:group JSONPatch
+{
+  // @dts-jest:pass:snap
+  testType<JSONPatch>([
+    { op: 'add', path: '/a/b/c', value: { someKey: 'someValue' } },
+    { op: 'remove', path: '/x/y/z' },
+    { op: 'replace', path: '/g/h/i', value: null },
+    { op: 'move', from: '/j/k/l', path: '/m/n/o' },
+    { op: 'copy', from: '/p/q/r', path: '/s/t/u' },
+    { op: 'test', path: '/v/w/x', value: [1, 2, 3] },
+  ]);
+}
+
+// @dts-jest:group GeoJSONPoint
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONPoint>({
+    type: 'Point',
+    coordinates: [100.0, 0.0],
+  });
+}
+
+// @dts-jest:group GeoJSONLineString
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONLineString>({
+    type: 'LineString',
+    coordinates: [
+      [100.0, 0.0],
+      [101.0, 1.0],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONPolygon
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONPolygon>({
+    type: 'Polygon',
+    coordinates: [
+      [
+        [100.0, 0.0],
+        [101.0, 0.0],
+        [101.0, 1.0],
+        [100.0, 1.0],
+        [100.0, 0.0],
+      ],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONMultiPoint
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONMultiPoint>({
+    type: 'MultiPoint',
+    coordinates: [
+      [100.0, 0.0],
+      [101.0, 1.0],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONMultiLineString
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONMultiLineString>({
+    type: 'MultiLineString',
+    coordinates: [
+      [
+        [100.0, 0.0],
+        [101.0, 1.0],
+      ],
+      [
+        [102.0, 2.0],
+        [103.0, 3.0],
+      ],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONMultiPolygon
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONMultiPolygon>({
+    type: 'MultiPolygon',
+    coordinates: [
+      [
+        [
+          [102.0, 2.0],
+          [103.0, 2.0],
+          [103.0, 3.0],
+          [102.0, 3.0],
+          [102.0, 2.0],
+        ],
+      ],
+      [
+        [
+          [100.0, 0.0],
+          [101.0, 0.0],
+          [101.0, 1.0],
+          [100.0, 1.0],
+          [100.0, 0.0],
+        ],
+        [
+          [100.2, 0.2],
+          [100.8, 0.2],
+          [100.8, 0.8],
+          [100.2, 0.8],
+          [100.2, 0.2],
+        ],
+      ],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONGeometry - Point
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONGeometry>({
+    type: 'Point',
+    coordinates: [100.0, 0.0],
+  });
+}
+
+// @dts-jest:group GeoJSONGeometry - LineString
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONGeometry>({
+    type: 'LineString',
+    coordinates: [
+      [100.0, 0.0],
+      [101.0, 1.0],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONGeometry - Polygon
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONGeometry>({
+    type: 'Polygon',
+    coordinates: [
+      [
+        [100.0, 0.0],
+        [101.0, 0.0],
+        [101.0, 1.0],
+        [100.0, 1.0],
+        [100.0, 0.0],
+      ],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONGeometry - MultiPoint
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONGeometry>({
+    type: 'MultiPoint',
+    coordinates: [
+      [100.0, 0.0],
+      [101.0, 1.0],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONGeometry - MultiLineString
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONGeometry>({
+    type: 'MultiLineString',
+    coordinates: [
+      [
+        [100.0, 0.0],
+        [101.0, 1.0],
+      ],
+      [
+        [102.0, 2.0],
+        [103.0, 3.0],
+      ],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONGeometry - MultiPolygon
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONGeometry>({
+    type: 'MultiPolygon',
+    coordinates: [
+      [
+        [
+          [102.0, 2.0],
+          [103.0, 2.0],
+          [103.0, 3.0],
+          [102.0, 3.0],
+          [102.0, 2.0],
+        ],
+      ],
+      [
+        [
+          [100.0, 0.0],
+          [101.0, 0.0],
+          [101.0, 1.0],
+          [100.0, 1.0],
+          [100.0, 0.0],
+        ],
+        [
+          [100.2, 0.2],
+          [100.8, 0.2],
+          [100.8, 0.8],
+          [100.2, 0.8],
+          [100.2, 0.2],
+        ],
+      ],
+    ],
+  });
+}
+
+// @dts-jest:group GeoJSONFeature with PointProperties
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONFeature<PointProperties>>({
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [125.6, 10.1],
+    },
+    properties: {
+      name: 'Dinagat Islands',
+      elevation: 357,
+    },
+  });
+}
+
+// @dts-jest:group GeoJSONFeature with AreaProperties
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONFeature<AreaProperties>>({
+    type: 'Feature',
+    geometry: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [125.0, 10.0],
+          [125.0, 11.0],
+          [126.0, 11.0],
+          [126.0, 10.0],
+          [125.0, 10.0],
+        ],
+      ],
+    },
+    properties: {
+      name: 'Central Park',
+      area: 3416,
+      protected: true,
+    },
+  });
+}
+
+// @dts-jest:group GeoJSONFeatureCollection with PointProperties
+{
+  // @dts-jest:pass:snap
+  testType<GeoJSONFeatureCollection<PointProperties>>({
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [102.0, 0.5],
+        },
+        properties: {
+          name: 'Null Island',
+        },
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [103.0, 1.5],
+        },
+        properties: {
+          name: 'One-and-a-half Island',
+          elevation: 15,
+        },
+      },
+    ],
+  });
+}
+
+// @dts-jest:group HTTPRequestBody - simple data
+{
+  // @dts-jest:pass:snap
+  testType<HTTPRequestBody>({
+    data: simpleJSONData,
+  });
+
+  // @dts-jest:pass:snap
+  testType<HTTPRequestBody>({
+    data: complexJSONData,
+  });
+}
+
+// @dts-jest:group HTTPResponse - simple JSON response
+{
+  // @dts-jest:pass:snap
+  testType<HTTPResponse<JSONObject>>({
+    status: 200,
+    statusText: 'OK',
+    headers: { 'Content-Type': 'application/json' },
+    body: simpleJSONData,
+  });
+}
+
+// @dts-jest:group HTTPResponse - complex JSON response
+{
+  // @dts-jest:pass:snap
+  testType<HTTPResponse<JSONObject>>({
+    status: 200,
+    statusText: 'OK',
+    headers: { 'Content-Type': 'application/json' },
+    body: complexJSONData,
+  });
+}
+
+// @dts-jest:group HTTPResponse - typed response
+{
+  // Simulating a more structured response body
+  const userResponseBody: UserResponse = {
+    id: 1,
+    username: 'john_doe',
+    profile: { age: 30, active: true },
+  };
+
+  // @dts-jest:pass:snap
+  testType<HTTPResponse<UserResponse>>({
+    status: 200,
+    statusText: 'OK',
+    headers: { 'Content-Type': 'application/json' },
+    body: userResponseBody,
+  });
 }
